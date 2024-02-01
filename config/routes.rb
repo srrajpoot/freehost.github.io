f5bf776873d2117162b37db965146199e1d5ada3
srrajpoot <srrajpoot01@gmail.com>
2024-02-01 19:38:25 +0530

    first commit github desktop
    lkuh

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+   get 'getsde/index'
+  # get '*path' => redirect('/')
+  root "render#index"
+
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+end
